4576c8cd8a1671647eb2dcecea1cfa709b1d8ae5
Christian Pradelli <c.pradelli@sig2k.com>
2021-04-08 15:17:03 -0300

    remove log

--- BlazorZXingJs/wwwroot/MultiFormatReader.js
+++ BlazorZXingJs/wwwroot/MultiFormatReader.js
@@ -134,8 +134,6 @@ export async function setVideoProperties(mediaTrackConstraints) {
             if (typeof track.getCapabilities == 'function') {
                 capabilities = track.getCapabilities();
 
-                console.log('Get track Capabilities ', capabilities);
-
                 if (mediaTrackConstraints) {
                     await setMediaTrackConstraints(capabilities, track, mediaTrackConstraints);
                     console.log('Set MediaTrackConstraints ', mediaTrackConstraints);
